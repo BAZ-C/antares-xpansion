16c54b70935ee41dbf49c82bd2cdb5a02a866c97
Jason Marechal <jason.marechal+github@gmail.com>
2022-11-09 10:49:24 +0100

    Clean up

--- src/cpp/lpnamer/problem_modifier/MyAdapter.cpp
+++ src/cpp/lpnamer/problem_modifier/MyAdapter.cpp
@@ -11,40 +11,10 @@
 #include "VariableFileReader.h"
 #include "helpers/StringUtils.h"
 #include "solver_utils.h"
-std::istringstream MyAdapter::reader_extract(const ProblemData& problemData,
-                                             ArchiveReader& reader) const {
-  auto variableFileContent =
-      reader.ExtractFileInStringStream(problemData._variables_txt);
-  return variableFileContent;
-}
 void MyAdapter::reader_extract_file(const std::string& problem_name,
                                     ArchiveReader& reader,
                                     std::filesystem::path lpDir) const {
   reader.ExtractFile(problem_name, lpDir);
-}
-
-void MyAdapter::extract_variables(
-    std::istringstream& variableFileContent,
-    std::vector<std::string>& var_names,
-    std::map<colId, ColumnsToChange>& p_ntc_columns,
-    std::map<colId, ColumnsToChange>& p_direct_cost_columns,
-    std::map<colId, ColumnsToChange>& p_indirect_cost_columns,
-    const std::vector<ActiveLink>& links,
-    ProblemGenerationLog::ProblemGenerationLoggerSharedPointer logger) const {
-  // List of variables
-  VariableFileReadNameConfiguration variable_name_config;
-  variable_name_config.ntc_variable_name = "ValeurDeNTCOrigineVersExtremite";
-  variable_name_config.cost_origin_variable_name =
-      "CoutOrigineVersExtremiteDeLInterconnexion";
-  variable_name_config.cost_extremite_variable_name =
-      "CoutExtremiteVersOrigineDeLInterconnexion";
-
-  auto variableReader = VariableFileReader(variableFileContent, links,
-                                           variable_name_config, logger);
-  var_names = variableReader.getVariables();
-  p_ntc_columns = variableReader.getNtcVarColumns();
-  p_direct_cost_columns = variableReader.getDirectCostVarColumns();
-  p_indirect_cost_columns = variableReader.getIndirectCostVarColumns();
 }
 
 std::shared_ptr<Problem> MyAdapter::provide_problem(
--- src/cpp/lpnamer/problem_modifier/MyAdapter.h
+++ src/cpp/lpnamer/problem_modifier/MyAdapter.h
@@ -10,19 +10,9 @@ class MyAdapter : public IProblemProviderPort {
  public:
   explicit MyAdapter(std::filesystem::path lp_dir, const std::string& data,
                      std::shared_ptr<ArchiveReader> ptr);
-  std::istringstream reader_extract(const ProblemData& problemData,
-                                    ArchiveReader& reader) const;
   void reader_extract_file(const std::string& problem_name,
                            ArchiveReader& reader,
                            std::filesystem::path lpDir) const;
-  void extract_variables(
-      std::istringstream& variableFileContent,
-      std::vector<std::string>& var_names,
-      std::map<colId, ColumnsToChange>& p_ntc_columns,
-      std::map<colId, ColumnsToChange>& p_direct_cost_columns,
-      std::map<colId, ColumnsToChange>& p_indirect_cost_columns,
-      const std::vector<ActiveLink>& links,
-      ProblemGenerationLog::ProblemGenerationLoggerSharedPointer logger) const;
   const std::filesystem::path lp_dir_;
   const std::string problem_name;
   [[nodiscard]] std::shared_ptr<Problem> provide_problem(
